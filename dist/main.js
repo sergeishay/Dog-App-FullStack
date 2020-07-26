719f8a4766ac5d87781a76de4febe844f06a5de7
ameliawalcek <ameliawalcek@gmail.com>
2020-07-26 18:23:45 +0300

    more map stuff

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
-const apiManager = new APIManager()
+const apiManager = new APIManager();
+const renderer = new Renderer();
 
-function initMap() {
+const initMap = () => {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         streetViewControl: false,
@@ -55,102 +56,4 @@ function initMap() {
             infowindow.open(marker.get("map"), marker);
         });
     }
-
-
-    // google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
-    //     return function () {
-
-    //         infowindow.setContent(content);
-    //         infowindow.open(map, marker);
-    //     }
-    // })(marker, contentString, infowindow))
-
 }
-
-
-//     var infowindow = new google.maps.InfoWindow({
-//         content: contentString
-//     });
-
-
-//     var infowindow2 = new google.maps.InfoWindow({
-//         content: contentString2
-//     });
-
-//     var marker = new google.maps.Marker({
-//         position: uluru,
-//         map: map,
-//         title: 'Dog 1',
-//         icon: "assets/download1.png"
-//     });
-
-//     var marker2 = new google.maps.Marker({
-//         position: uluru2,
-//         map: map,
-//         title: 'Dog 2',
-//         icon: "assets/download1.png"
-//     });
-
-//     marker.addListener('click', function () {
-//         infowindow.open(map, marker);
-//     });
-
-//     marker2.addListener('click', function () {
-//         infowindow2.open(map, marker2);
-//     });
-// }
-
-
-// mapInfo = {
-//     user: {
-//         _id: 12345,
-//         lat: 32.0784,
-//         lon: 34.815,
-//         dog: {
-//             name: "Lucky",
-//             breed: "husky",
-//             picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
-//             toy: "ball",
-//             treat: "bone"
-//         }
-
-//     },
-//     otherUsers: [
-//         {
-//             _id: 2345,
-//             lat: 32.0783,
-//             lon: 34.814,
-//             dog: {
-//                 name: "Lucky",
-//                 breed: "husky",
-//                 picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
-//                 toy: "ball",
-//                 treat: "bone"
-//             }
-//         }
-//         , {
-//             _id: 2346,
-//             lat: 32.0783,
-//             lon: 34.816,
-//             dog: {
-//                 name: "Lucky",
-//                 breed: "husky",
-//                 picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
-//                 toy: "ball",
-//                 treat: "bone"
-//             }
-//         },
-//         {
-//             _id: 2347,
-//             lat: 32.0783,
-//             lon: 34.815,
-//             dog: {
-//                 name: "Lucky",
-//                 breed: "husky",
-//                 picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
-//                 toy: "ball",
-//                 treat: "bone"
-//             }
-//         },
-//     ]
-// }}
