273538296b9523f44b91afc8f1ab964622a68032
ameliawalcek <ameliawalcek@gmail.com>
2020-07-26 13:50:15 +0300

    handlebars

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+const renderer = new Renderer()
 // Chat Box Listeners-------------------------------------------------------
 $("#chat-circle").click(function () {
     $("#chat-circle").toggle('scale');
@@ -10,3 +11,60 @@ $(".chat-box-toggle").click(function () {
 })
 
 $('select').material_select();
+
+
+mapInfo = {
+    user: {
+        _id: 12345,
+        lat: 32.0784,
+        lon: 34.815,
+        dog: {
+            name: "Lucky",
+            breed: "husky",
+            picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
+            toy: "ball",
+            treat: "bone"
+        }
+
+    },
+    otherUsers: [
+        {
+            _id: 2345,
+            lat: 32.0783,
+            lon: 34.814,
+            dog: {
+                name: "Lucky",
+                breed: "husky",
+                picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
+                toy: "ball",
+                treat: "bone"
+            }
+        }
+        , {
+            _id: 2346,
+            lat: 32.0783,
+            lon: 34.816,
+            dog: {
+                name: "Lucky",
+                breed: "husky",
+                picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
+                toy: "ball",
+                treat: "bone"
+            }
+        },
+        {
+            _id: 2347,
+            lat: 32.0783,
+            lon: 34.815,
+            dog: {
+                name: "Lucky",
+                breed: "husky",
+                picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
+                toy: "ball",
+                treat: "bone"
+            }
+        },
+    ]
+}
+
+renderer.renderData(mapInfo)
--- dist/renderer.js
+++ dist/renderer.js
@@ -1,29 +1,10 @@
 class Renderer {
     renderData = mapInfo => {
-        $('#container').empty()
+        $('#map').empty()
         const source = $('#map-template').html()
         const template = Handlebars.compile(source)
         const newHTML = template({ mapInfo })
 
-        $('#container').append(newHTML)
-    }
-}
-
-dogInfo = {
-    user: {
-        _id: 12345,
-        lat: 32.0784,
-        lon: 34.815,
-        dog: {
-            name: "Lucky",
-            breed: "husky",
-            picture: "http://cdn.akc.org/content/article-body-image/newfoundland_dog_pictures.jpg",
-            toy: "ball",
-            treat: "bone"
-        }
-
-    },
-    otherUsers: {
-        
+        $('#map').append(newHTML)
     }
 }
