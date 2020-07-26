5a4c600e759f394704cd874108aa3f0ce5b47065
myusername <Matanfriedleet@hotmail.com>
2020-07-26 11:27:54 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require("path")
+const app = express();
+const user = require('./server/routes/user');
+const event = require('./server/routes/event');
+const dog = require('./server/routes/dog');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dog-app', { useNewUrlParser: true });
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/user', user);
+app.use('/users', user);
+app.use('/event', event);
+app.use('/events', event);
+app.use('/dog', dog);
+app.use('/dogs', dog);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Running server on port ${ port }`));
