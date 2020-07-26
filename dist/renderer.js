d5222e0fcb067a6a6164d4ff0f8eace6c9e2cb86
ameliawalcek <ameliawalcek@gmail.com>
2020-07-26 19:40:42 +0300

    more render

--- dist/renderer.js
+++ dist/renderer.js
@@ -10,7 +10,28 @@ class Renderer {
     renderNonAuthNav = (string) => {
         this.makeHandlebar("#nonAuthNav-template", '#navbar-container', string)
     }
+
+    renderLandingPage = (string) => {
+        this.makeHandlebar("#home-template", '#main-container', string)
+    }
+
+    renderLogin = (string) => {
+        this.makeHandlebar("#login-template", '#main-container', string)
+    }
+
+    renderProfile = (user) => {
+        this.makeHandlebar("#profile-template", '#main-container', user)
+    }
+
+    renderEvent = (events) => {
+        this.makeHandlebar("#events-template", '#main-container', events)
+    }
+
+    renderEventForm = (events) => {
+        this.makeHandlebar("#eventsForm-template", '#main-container', events)
+    }
 }
+
 Renderer.prototype.makeHandlebar = (templateId, containerId, data) => {
     const source = $(templateId).html();
     const template = Handlebars.compile(source);
